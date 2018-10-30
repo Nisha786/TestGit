706a9610d875c4037532f951ef24edf18a0c571c
Nirmala <vaishnav.nima12@gmail.com>
2018-10-30 10:33:45 +0530

    changes done by arun rathore

--- AirportWeb/src/main/java/com/mavenDemo/controller/LoginController.java
+++ AirportWeb/src/main/java/com/mavenDemo/controller/LoginController.java
@@ -6,19 +6,15 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.swing.plaf.synth.SynthSeparatorUI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.mavenDemo.Model.UserDTO;
 import com.mavenDemo.service.LoginService;
-import com.mavenDemo.service.RegistrationService;
 
 @Controller
 public class LoginController {
@@ -31,7 +27,7 @@ public class LoginController {
 		System.out.println("++++++++Login page++++++++");
  		map.put("loginData", new UserDTO());
 		UserDTO userDto = new UserDTO();
-		System.out.println("++++++++Login page return++++++++");
+		System.out.println("++++++++mmmmmmmmmmSOP is added by Nirmala Deviji++++++++");
 		
 		System.out.println("++++++++SOP is added by Nirmala Deviji++++++++");
 		System.out.println("++++++++SOP is added by Rahul ++++++++");
